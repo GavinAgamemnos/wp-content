de167ddb95c621629b42362de4ba9b1dcd29a6fc
Gavin <gavin.agamemnos@gmail.com>
2022-11-15 17:35:24 +0000

    Final commit, added checking for existance of fields before ouputting them

--- themes/tech/templates/product-details.php
+++ themes/tech/templates/product-details.php
@@ -54,7 +54,7 @@ $awardsInfo = $BookDetails['awards_content'];
 				<!--Book cover image column-->
 
 			  <div class="col-12 col-md-6 cover-image">
-					<img src="<?php echo $image; ?>" alt="Book Cover">
+					<?php if( $image ) : ?><img src="<?php echo $image;?>" alt="Book Cover"><?php endif;?>
 						<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 			 				viewBox="0 0 257 257" style="enable-background:new 0 0 257 257;" xml:space="preserve" class="ball">
 							<circle cx="128.5" cy="128.5" r="128.5" style="fill: #efefef	"/>
@@ -65,21 +65,21 @@ $awardsInfo = $BookDetails['awards_content'];
 
 				<div class="col-12 col-md-6 product-details">
 					<header>
-						<span class="release"><strong>Release date:</strong> <?php echo $createSaleDate->format('d-m-Y'); ?></span>
-						<h1><?php echo $title; ?></h1>
-						<h2><?php echo $author; ?></h2>
+						<?php if( $createSaleDate ) :?><span class="release"><strong>Release date:</strong>  <?php echo $createSaleDate->format('d-m-Y');?></span><?php endif;?>
+						<?php if( $title ) :?><h1><?php echo $title; ?></h1><?php endif;?>
+						<?php if( $author ) :?><h2><?php echo $author; ?></h2><?php endif;?>
 					</header>
 
 					<?php echo $bio; ?>
 
 					<div class="prices">
-						CAD: <span class="price"><?php echo $priceCAD; ?></span>
-			      USD: <span class="price"><?php echo $priceUSD; ?></span>
+						<?php if( $priceCAD ) :?>CAD: <span class="price"><?php echo $priceCAD; ?></span><?php endif;?>
+			      <?php if( $priceUSD ) :?>USD: <span class="price"><?php echo $priceUSD; ?></span><?php endif;?>
 					</div>
 
 					<div class="row buy">
 						<div class="col-6">
-							<a class="btn btn-secondary btn-buy" href="<?php echo $buyLinkURL;?>"><?php echo $buyLinkText;?></a>
+							<a class="btn btn-secondary btn-buy" href="<?php if( $buyLinkURL ) : echo $buyLinkURL; endif;?>"><?php if( $buyLinkText ) : echo $buyLinkText; endif;?></a>
 						</div>
 
 						<div class="col-6">
@@ -93,29 +93,34 @@ $awardsInfo = $BookDetails['awards_content'];
 								 Other Retailers
 							 </button>
 
-							 <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
 								 <?php
-									 foreach( $retailers as $retailer ) :
-										 $retailerName = $retailer['label'];
-											echo '<li class="dropdown-item">' . $retailerName. '<li>';
+								 if( $retailers ) :
+									 echo '<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">';
+										 foreach( $retailers as $retailer ) :
+											 $retailerName = $retailer['label'];
+												echo '<li class="dropdown-item">' . $retailerName. '<li>';
 											endforeach;
+										echo '</ul>';
+									endif;
 								 ?>
-							 </ul>
 						 </div>
 						</div>
 
 					</div>
 
 					<h3>Other formats:</h3>
-					<ul class="formats">
+
 						<?php
+						if( $formats ) :
+							echo '<ul class="formats">';
 							foreach( $formats as $format ) :
 									if(is_array($format)){
 										 echo '<li>' . $format["detail"]. '</li>';
 								 }
 							endforeach;
+							echo '</ul>';
+						endif;
 							?>
-					</ul>
 			  </div>
 			</div>
 
@@ -123,46 +128,53 @@ $awardsInfo = $BookDetails['awards_content'];
 
 			<div class="tabs">
 
-			  <input class="radiotab" name="tabs" tabindex="0" type="radio" id="description" checked="checked">
-			  <label class="label" for="description">Book Description</label>
-			  <div class="panel" tabindex="0">
-			    <h2>Book Description</h2>
-			    <?php echo $description; ?>
-			  </div>
+				<?php if( $description ) :?>
+				  <input class="radiotab" name="tabs" tabindex="0" type="radio" id="description" checked="checked">
+				  <label class="label" for="description">Book Description</label>
+				  <div class="panel" tabindex="0">
+				    <h2>Book Description</h2>
+				    <?php echo $description; ?>
+				  </div>
+				<?php endif;?>
 
-			  <input class="radiotab" tabindex="0" name="tabs" type="radio" id="details">
-			  <label class="label" for="details">Product Details</label>
-			  <div class="panel" tabindex="0">
-			    <h2>Product Details</h2>
-			    <?php echo $productInfo; ?>
-			  </div>
+				<?php if( $productInfo ) :?>
+				  <input class="radiotab" tabindex="0" name="tabs" type="radio" id="details">
+				  <label class="label" for="details">Product Details</label>
+				  <div class="panel" tabindex="0">
+				    <h2>Product Details</h2>
+				    <?php echo $productInfo;?>
+				  </div>
+				<?php endif;?>
 
-			  <input class="radiotab" tabindex="0" name="tabs" type="radio" id="reviews">
-			  <label class="label" for="reviews" >Reviews</label>
-			  <div class="panel" tabindex="0">
-			    <h2>Reviews</h2>
+				<?php if( $reviews ) :?>
+				  <input class="radiotab" tabindex="0" name="tabs" type="radio" id="reviews">
+				  <label class="label" for="reviews" >Reviews</label>
+				  <div class="panel" tabindex="0">
+				    <h2>Reviews</h2>
+								<?php foreach( $reviews as $review ) :
+									foreach ($review as $reviewData):
 
-						<?php
-							foreach( $reviews as $review ) :
-								foreach ($review as $reviewData):
-
-									if(is_array($reviewData)){
-										 echo '<div class="review">';
-										 echo '<p>' . $reviewData["description"]. '</p>';
-										 echo '<p>' . $reviewData["reviewer"]. '</p>';
-										 echo '</div>';
-								 }
+										if(is_array($reviewData)){
+											 echo '<div class="review">';
+											 echo '<p>' . $reviewData["description"]. '</p>';
+											 echo '<p>' . $reviewData["reviewer"]. '</p>';
+											 echo '</div>';
+									 }
+									endforeach;
 								endforeach;
-							endforeach;
-							?>
-			  </div>
+								?>
+				  </div>
+				<?php endif; ?>
 
-				<input class="radiotab" tabindex="0" name="tabs" type="radio" id="awards">
-			  <label class="label" for="awards">Awards</label>
-			  <div class="panel" tabindex="0">
-				  <h2>Awards</h2>
-				  <?php echo $awardsInfo; ?>
-			  </div>
+				<?php if( $reviews ) :?>
+					<input class="radiotab" tabindex="0" name="tabs" type="radio" id="awards">
+				  <label class="label" for="awards">Awards</label>
+				  <div class="panel" tabindex="0">
+					  <h2>Awards</h2>
+					  <?php echo $awardsInfo; ?>
+				  </div>
+				<?php endif;?>
+						
 			</div>
 
 	</div>
